6db324155b910f9153f27ef5d8d519ee8df47d6a
mark4z <anymre@outlook.com>
2021-03-08 22:24:55 +0800

    raft

--- php/www/db_config.php
+++ php/www/db_config.php
@@ -1,6 +1,6 @@
 <?php
 	$db_url = "server.anymre.top"; // 数据库服务器地址
-    $db_port = "6379";
+    $db_port = "8926";
 	$db_user = "root"; // 数据库账号
 	$db_pwd = "c1234567"; // 数据库密码
 	$db_name = "php"; // 数据库名称
