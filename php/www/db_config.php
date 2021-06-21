4e442ec5359cba00716cb29b77fefcf092bed9d1
mark4z <anymre@outlook.com>
2021-06-21 14:56:01 +0800

    es

--- php/www/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$db_url = "server.anymre.top"; // 数据库服务器地址
-    $db_port = "8926";
-	$db_user = "root"; // 数据库账号
-	$db_pwd = "c1234567"; // 数据库密码
-	$db_name = "php"; // 数据库名称
-	$appid = "wxe09a991d27bfe822"; // 公众号appid
-	$appsecret = "6d2f6fcb7d0bd0bb74e11b8a2e20d545"; // 公众号appsecret
-?>
